9a2a8d7a054dba6d1272c80f713f54ccfbf08e8e
Mikhail <mishken3@gmail.com>
2022-06-06 15:07:48 +0300

    moved Todo to components folder & fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { store } from './redux/index';
 import './App.css';
 
-import { Todo } from './Todo';
+import { Todo } from './components/Todo';
 
 function App() {
 	return (
