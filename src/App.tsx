74181cbfd9792cd506494cf209a79a24e9aa5a8c
Mikhail <mishken3@gmail.com>
2022-06-07 12:31:24 +0300

    add header comp & connect bootstrap css to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/index';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Todo } from './components/Todo';
+import { Header, Todo } from './components';
 
 function App() {
 	return (
 		<Provider store={store}>
+			<Header />
 			<div className="App">
 				<p>App</p>
 				<Todo />
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+export const Header = () => {
+	return (
+		<Navbar bg="primary" variant="dark">
+			<Container className="justify-content-center">
+				<Navbar.Brand>To-do-do-do app</Navbar.Brand>
+			</Container>
+		</Navbar>
+	);
+};
