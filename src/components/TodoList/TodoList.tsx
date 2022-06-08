eb4632e0dee0302005450589a68d7acd06af303b
Mikhail <mishken3@gmail.com>
2022-06-08 18:29:00 +0300

    add TodoList Component

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { TodoType } from '../../redux/reducer/todo/types';
+import { Todo } from '../index';
+
+export const TodoList = () => {
+	const todos: TodoType[] = useTypedSelector((store) => store.todos);
+
+	if (!todos.length) {
+		return (
+			<div className="d-flex align-items-center flex-column">
+				<h2 className="pb-20">Активных задач нет.</h2>
+				<p>Добавьте задачку в поле выше.</p>
+			</div>
+		);
+	}
+
+	return (
+		<div className="d-flex flex-column-reverse align-items-center justify-content-center ">
+			{todos.map((todo) => (
+				<Todo key={todo.id} todo={todo} />
+			))}
+		</div>
+	);
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './Header';
 export * from './Todo';
 export * from './AddTodo';
+export * from './TodoList';
